f750c76ae1c7157d8c426cce335cbcfd7365e2dd
chaofengc <chaofenghust@gmail.com>
2022-02-28 19:42:06 +0800

    Fix compatible with pytorch 1.8

--- pyiqa/data/transforms.py
+++ pyiqa/data/transforms.py
@@ -33,7 +33,7 @@ class PairedRandomCrop(tf.RandomCrop):
         if self.padding is not None:
             img = F.pad(img, self.padding, self.fill, self.padding_mode)
 
-        width, height = F.get_image_size(img)
+        width, height = img.size 
         # pad the width if needed
         if self.pad_if_needed and width < self.size[1]:
             padding = [self.size[1] - width, 0]
