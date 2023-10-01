f512ab763479c465022c39734d47a66a6c6c9388
chaofengc <chaofenghust@gmail.com>
2023-10-01 17:55:35 +0800

    docs: :memo: update docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -53,6 +53,8 @@ extensions = [
    'recommonmark',
  ]
 
+autosummary_generate = True  # Turn on sphinx.ext.autosummary
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
