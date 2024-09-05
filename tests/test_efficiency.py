e4a4945dbe10b00f0f52747b17769fb0f3b7adf9
chaofengc <chaofenghust@gmail.com>
2024-09-05 17:06:14 +0800

    feat: :sparkles: add gpu memory benchmark

--- tests/test_efficiency.py
+++ tests/test_efficiency.py
@@ -4,21 +4,25 @@ from time import time
 import pandas as pd
 import numpy as np
 from tqdm import tqdm
+import torch
+import gc
 
 
 def time_benchmark(device):
-    save_file = './tests/time_benchmark.csv'
+    save_file = './tests/Efficiency_benchmark.csv'
     time_sum = pd.read_csv(save_file, index_col='Method') 
+    time_sum = time_sum.drop('peak GPU memory (GB)', axis=1)
 
     file_list = scandir_images('./tests/test_img_dir_1080p') 
     exclude_filters = ['fid', 'inception_score', 'clipscore', 'topiq_nr-face']
     if device == 'cpu':
         exclude_filters += ['qalign', 'qalign_4bit', 'qalign_8bit']
     metric_list = pyiqa.list_models(exclude_filters=exclude_filters)
-    
+
     pbar = tqdm(total=len(metric_list))
     for idx, metric_name in enumerate(metric_list):
 
+        torch.cuda.reset_peak_memory_stats()
         metric_func = pyiqa.create_metric(metric_name, device=device)
 
         process_time = []
@@ -29,10 +33,17 @@ def time_benchmark(device):
             metric_func(img_path, img_path)
             process_time.append(time() - start_time)
         
+        # Get the peak memory allocated
+        peak_memory = torch.cuda.max_memory_allocated() / 1024**3  # Convert to GB 
+        
         pbar.update(1)
+        del metric_func
+        torch.cuda.empty_cache()
+        gc.collect()
 
         avg_process_time = np.mean(process_time[1:])
         time_sum.loc[metric_name, device] = float(f'{avg_process_time:.4f}')
+        time_sum.loc[metric_name, 'Peak GPU Mem (GB)'] = float(f'{peak_memory:.4f}')
 
     pbar.close()
     time_sum = time_sum.sort_values(by=['cuda'], ascending=True)
