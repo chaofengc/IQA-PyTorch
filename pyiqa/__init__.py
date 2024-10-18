9a483918970f14277484916c4ade21cd7a327425
chaofengc <chaofenghust@gmail.com>
2024-10-18 19:18:42 +0800

    perf: :zap: remove unnecessary imports to make initialization much faster

--- pyiqa/__init__.py
+++ pyiqa/__init__.py
@@ -1,13 +1,3 @@
 # flake8: noqa
-from .archs import *
-from .data import *
-from .losses import *
-from .metrics import *
-from .models import *
-from .test import *
-from .train import *
-from .utils import *
-from .matlab_utils import *
 from .api_helpers import *
-from .default_model_configs import *
 from .version import __gitsha__, __version__
