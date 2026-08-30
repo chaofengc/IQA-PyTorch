451a24d7c122ffba30ce8a0f27709d21f64e2d6f
et21ff <et21ff@users.noreply.github.com>
2026-08-30 15:28:19 +0000

    test(color): enforce unit-range YIQ equivalence

--- tests/test_brisque_optimizations.py
+++ tests/test_brisque_optimizations.py
@@ -16,3 +16,6 @@ def test_y_channel_matches_full_yiq_first_channel_after_rounding():
 
 def test_y_channel_matches_full_yiq_at_unit_range():
     image = torch.rand(2, 3, 16, 16)
+    expected = rgb2yiq(image)[:, [0]]
+
+    torch.testing.assert_close(to_y_channel(image, 1), expected)
